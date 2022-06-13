b58e9cff75311f80342cf137541ce83da380ff89
Anas <anas.khan@hotmail.co.uk>
2022-06-13 17:15:19 +0100

    Final Version

---
+++ src/Gallery.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const STATUS_FETCHING = "fetching";
+const STATUS_FETCHED = "fetched";
+const STATUS_LOADED = "loaded";
+
+export default class extends React.Component {
+  state = {
+    images: [],
+    loadingState: STATUS_FETCHING,
+    page: 0,
+    limit: 6
+  };
+
+  componentDidMount() {
+    this.getRandomCat();
+  }
+
+  loadMore = () => {
+    this.setState(
+      (s) => ({
+        page: s.page + 1
+      }),
+      () => {
+        fetch(
+          `https://api.thecatapi.com/v1/images/search?limit=${this.state.limit}&page=${this.state.page}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "x-api-key": "4bebae0d-0ec4-4787-8e77-8602741525af"
+            }
+          }
+        )
+          .then((data) => data.json())
+          .then((data) => {
+            this.setState({
+              images: [
+                ...this.state.images,
+                ...data.map((e) => ({ id: e.id, url: e.url }))
+              ]
+            });
+          });
+      }
+    );
+  };
+
+  getRandomCat = () => {
+    this.setState({
+      loadingState: STATUS_FETCHING
+    });
+    fetch(
+      `https://api.thecatapi.com/v1/images/search?limit=${this.state.limit}&page=${this.state.page}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": "4bebae0d-0ec4-4787-8e77-8602741525af"
+        }
+      }
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        this.setState({
+          images: data.map((e) => ({ id: e.id, url: e.url })),
+          loadingState: STATUS_FETCHED
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="gallery">
+        {this.state.loadingState !== STATUS_FETCHED && (
+          <div className="loader">Loading...</div>
+        )}
+        {this.state.loadingState !== STATUS_FETCHING &&
+          this.state.images.length > 0 &&
+          this.state.images.map((image) => (
+            <div className="imagePlaceholder" key={image.id}>
+              <img src={image.url} alt="Cat" />
+            </div>
+          ))}
+        {this.state.loadingState === STATUS_FETCHED && (
+          <button onClick={this.loadMore}>Load More</button>
+        )}
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Link } from "react-router-dom";
+import Content from "./Content";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/random">Random Cats</Link>
+            </li>
+            <li>
+              <Link to="/gallery">View Gallery</Link>
+            </li>
+          </ul>
+        </nav>
+        <Content />
+      </div>
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
